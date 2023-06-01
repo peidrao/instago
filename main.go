83ec9d1005d2348f8c9aa3848a7dd54b5ea0ee99
🐘 Pedro Fonseca <contatopedrorn@gmail.com>
2023-06-01 00:50:24 -0300

    refactor: main.go

--- main.go
+++ main.go
@@ -1,32 +1,14 @@
 package main
 
 import (
-	"os"
-
-	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
-	"github.com/peidrao/instago/src/controllers"
 	"github.com/peidrao/instago/src/database"
+	"github.com/peidrao/instago/src/routers"
 )
-
-func connectDB() {
-	dsn := "host=localhost user=" + os.Getenv("POSTGRES_USER") + " password=" + os.Getenv("POSTGRES_PASSWORD") + " dbname=" + os.Getenv("POSTGRES_DB") + " port=5432"
-	database.Connect(dsn)
-	database.Migrate()
-
-}
 
 func main() {
 	godotenv.Load()
-	connectDB()
-	router := initRouter()
+	database.ConnectDB()
+	router := routers.SetupRouter()
 	router.Run(":8080")
-}
-
-func initRouter() *gin.Engine {
-	router := gin.Default()
-	api := router.Group("/api")
-
-	api.POST("user/register", controllers.RegisterUser)
-	return router
 }
