452cab66e6367dc8cac7d7ae551f0131d666e58f
🐘 Pedro Fonseca <contatopedrorn@gmail.com>
2023-06-10 20:59:05 -0300

    user detail response

--- src/domain/responses/user.go
+++ src/domain/responses/user.go
@@ -1,7 +1,22 @@
 package responses
 
+import "time"
+
 type FollowUserResponse struct {
 	ID       uint   `json:"id"`
 	Username string `json:"username"`
 	FullName string `json:"full_name"`
+}
+
+type UserDetailResponse struct {
+	Username string `json:"username"`
+	FullName string `json:"full_name"`
+	Bio      string `json:"bio"`
+	Link     string `json:"link"`
+
+	Picture   string `json:"picture"`
+	Followers uint   `json:"followers"`
+	Following uint   `json:"following"`
+
+	CreatedAt time.Time `json:"created_at"`
 }
