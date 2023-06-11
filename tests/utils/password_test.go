cd27d05528cc31cc765fda7d45d0f00d80b6a849
🐘 Pedro Fonseca <contatopedrorn@gmail.com>
2023-06-11 12:44:04 -0300

    refactor: tests folder

--- tests/utils/password_test.go
+++ tests/utils/password_test.go
@@ -1,15 +1,16 @@
-package utils
+package tests
 
 import (
 	"testing"
 
+	"github.com/peidrao/instago/utils"
 	"github.com/stretchr/testify/require"
 )
 
 func TestPasswordHashed(t *testing.T) {
-	password := GenerateRandomString(8)
+	password := utils.GenerateRandomString(8)
 
-	hashedPassword, err := HashPassword(password)
+	hashedPassword, err := utils.HashPassword(password)
 
 	require.NoError(t, err)
 	require.NotEmpty(t, hashedPassword)
