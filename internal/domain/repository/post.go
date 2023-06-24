c8db94645a0b80fd4126c46aab6e4e910aa4127c
🐘 Pedro Fonseca <contatopedrorn@gmail.com>
2023-06-24 20:18:00 -0300

    update post entity

--- internal/domain/repository/post.go
+++ internal/domain/repository/post.go
@@ -15,6 +15,31 @@ func NewPostRepository(db *gorm.DB) *PostRepository {
 	}
 }
 
-func (f *PostRepository) CreatePost(post *entity.Post) error {
-	return f.Create(&post)
+func (p *PostRepository) CreatePost(post *entity.Post) error {
+	return p.Create(&post)
+}
+
+func (p *PostRepository) FindPostsByUser(ID uint) ([]entity.Post, error) {
+	var posts []entity.Post
+
+	attr := map[string]interface{}{"user_id": ID}
+	err := p.FindByAttr(&posts, attr)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return posts, nil
+}
+
+func (p *PostRepository) FindPostByID(ID uint) (*entity.Post, error) {
+	var post entity.Post
+
+	err := p.GetByID(&post, ID)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &post, nil
 }
