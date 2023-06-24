3199f779946d8f6fd251d52bea0a893a43b09411
🐘 Pedro Fonseca <contatopedrorn@gmail.com>
2023-06-24 11:26:12 -0300

    update follow entity

--- internal/domain/entity/follow.go
+++ internal/domain/entity/follow.go
@@ -3,10 +3,10 @@ package entity
 import "time"
 
 type Follow struct {
-	ID          uint      `gorm:"primaryKey"`
-	FollowerID  uint      `gorm:"index"`
-	FollowingID uint      `gorm:"index"`
-	IsPrivate   bool      `gorm:"default:false"`
+	ID          uint `gorm:"primaryKey"`
+	FollowerID  uint `gorm:"index"`
+	FollowingID uint `gorm:"index"`
+	IsAccept    bool
 	IsActive    bool      `gorm:"default:true"`
 	CreatedAt   time.Time `gorm:"autoCreateTime"`
 	UpdatedAt   time.Time `gorm:"autoUpdateTime"`
