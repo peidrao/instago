663627992139de1e1b87b0065b74dc4d95bb2fd9
🐍 Pedro Fonseca <contatopedrorn@gmail.com>
2023-10-03 21:03:40 -0400

    fmt in main

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,16 +1,25 @@
 package main
 
 import (
+	"log"
+
 	"github.com/joho/godotenv"
 	"github.com/peidrao/instago/internal/infrastructure"
 	"github.com/peidrao/instago/internal/interfaces/api/routers"
 )
 
 func main() {
-	godotenv.Load()
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
 
 	db := infrastructure.Init()
 
 	router := routers.SetupRouter(db)
-	router.Run(":8080")
+	err = router.Run(":8080")
+
+	if err != nil {
+		log.Fatalf("Error starting server: %v", err)
+	}
 }
